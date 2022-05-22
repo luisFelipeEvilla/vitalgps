47f433e8e8d39ced80a6a27466cb5fae27210818
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-05-21 20:01:24 -0500

    agregado paginación en la homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import { Link, useNavigate  } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import products from '../../centers.json'
+import ReactPaginate from 'react-paginate';
 
 import './home.css'
 
 function HomeScreen(props) {
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 9;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(products.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(products.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage])
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % products.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
     const navigate = useNavigate();
 
     const handleClick = (nit) => {
         navigate(`/center/${nit}`)
     }
 
+    const index = 0;
+
     return (
         <div className="container">
-            
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 card-container">
                 {
-                    products.map(product =>
+                    currentItems.map(product =>
                         <div key={product.nits_nit} className="col">
                             <div className="card shadow-sm">
                                 <img onClick={(e) => handleClick(product.nits_nit)}
-                                src="https://lirp.cdn-website.com/a979b4f7/dms3rep/multi/opt/gente-caminando-sentada-edificio-hospital-exterior-cristal-clinica-ciudad-ilustracion-vector-plano-ayuda-medica-emergencia-arquitectura-concepto-salud_74855-10130-640w.jpg" className="card-img-top" alt="..." />
+                                    src="https://lirp.cdn-website.com/a979b4f7/dms3rep/multi/opt/gente-caminando-sentada-edificio-hospital-exterior-cristal-clinica-ciudad-ilustracion-vector-plano-ayuda-medica-emergencia-arquitectura-concepto-salud_74855-10130-640w.jpg" className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">{product.razon_social}</h5>
                                     <p className="card-subtitle mb-2 text-muted">Dirección: {product.direccion}</p>
@@ -33,6 +55,29 @@ function HomeScreen(props) {
                         </div>)
                 }
             </div>
+
+            <div className='pagination-container'>
+                <ReactPaginate
+                    className='pagination'
+                    pageClassName='page-item'
+                    pageLinkClassName='page-link'
+                    activeClassName='page-item active'
+                    nextClassName='page-item'
+                    previousClassName='page-item'
+                    nextLinkClassName='page-link'
+                    previousLinkClassName='page-link'
+                    breakClassName='page-item'
+                    breakLinkClassName='page-link'
+                    breakLabel="..."
+                    nextLabel="Siguiente"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel="Anterior"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+
         </div>
     )
 }
--- src/partials/header/index.js
+++ src/partials/header/index.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 import './header.css'
 
 const Header = () => (
-    <nav id="header" className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav id="header" className="navbar navbar-expand-lg">
         <div className="container-fluid">
             <Link className="navbar-brand" to="/">VitalGps</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+               <i className="bi bi-list navbar-toggler-icon"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="#" tabIndex="-1" aria-disabled="true">Login</Link>
-                </li>
-            </ul>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#" tabIndex="-1" aria-disabled="true">Login</Link>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
